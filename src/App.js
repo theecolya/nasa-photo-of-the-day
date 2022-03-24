07161aa3ee4650732315fb0dc116a30fbdf7ac3a
Colya Foxworth <cfox1106@gmail.com>
2022-03-24 15:40:03 -0500

    added styles using styled components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
 import NasaPhoto from "./Components/NasaPhoto";
+import styled from 'styled-components';
+
+const StyledBody = styled.div`
+    text-align: center;
+    background-color: rgba(182, 171, 199, 0.60);
+    border-radius: 20px;
+    width: 75%;
+    margin: 2em;
+`
 
 function App() {
   const [data, setData] = useState();
@@ -15,9 +24,9 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <StyledBody className="App">
       { data && <NasaPhoto photo={data} /> }
-    </div>
+    </StyledBody>
   );
 }
 
--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,13 +1,37 @@
 import React from "react";
+import styled from 'styled-components';
 
+const StyledImage = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+
+h3 {
+    margin-top: 2em;
+    padding-top: 1em;
+    text-shadow: 0px 3px 6px rgba(59, 59, 59);
+    text-transform: uppercase;
+} 
+img {
+    width: 60%;
+    border-radius: 10px;
+}
+p {
+    width: 60%;
+}
+hr {
+    width: 50%;
+}
+`
 const NasaPhoto = (props) => {
     return (
-        <div className="photo-container">
+        <StyledImage className="photo-container">
             <h3>{props.photo.title}</h3>
             <p>{props.photo.date}</p>
             <img src={props.photo.hdurl}/>
+            <hr></hr>
             <p>{props.photo.explanation}</p>
-        </div>
+        </StyledImage>
     )
 }
 
